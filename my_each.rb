7c747cd4070d80d1e76d7ce0e5946bf65b8e4c7f
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-10 18:52:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 collection = [1, 2, 3, 4]
 def my_each(collection)
+  if block_given?
   i = 0
   while i<collection.length
+    yield(collection[i])
     i=i+1
-  collection.map do |i|
-     i
+
 end
+collection
 end
 end
